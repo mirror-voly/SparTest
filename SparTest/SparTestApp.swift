a85158a0fa7851410fcc27c17307b173a9897c25
nur <77068320+mirror-voly@users.noreply.github.com>
2024-08-06 17:49:03 +0400

    Initial Commit

---
+++ SparTest/SparTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparTestApp.swift
+//  SparTest
+//
+//  Created by mix on 06.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SparTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
